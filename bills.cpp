ec8d3c4a9c6d856763ff76d90bbb35aa9dab3f5f
notarealone <alireza.hosseini.81@gmail.com>
2023-11-25 17:47:19 +0330

    Added extraction of resource prices for every month

--- bills.cpp
+++ bills.cpp
@@ -8,3 +8,14 @@
 #include <filesystem>
 #include "rapidcsv.h"
 
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+    rapidcsv::Document bills("bills.csv", rapidcsv::LabelParams(0, 0));
+
+    vector<int> gas_price = bills.GetColumn<int>("gas");
+    vector<int> elec_price = bills.GetColumn<int>("electricity");
+    vector<int> water_price = bills.GetColumn<int>("water");
+    
+}
