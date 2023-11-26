6c7b5be8b808473faf048710fa1bba36025588b8
notarealone <alireza.hosseini.81@gmail.com>
2023-11-26 22:49:33 +0330

    Added Pipe from children to parent

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -15,6 +16,12 @@ namespace fs = std::filesystem;
 #define BOLD_GREEN "\033[1;32m"
 #define BOLD_YELLOW "\033[1;33m"
 #define UNDERLINED_WHITE "\033[4;37m"
+
+#define WRITE_PIPE 1
+#define READ_PIPE 0
+
+#define MAX_LENGTH 100
+#define BUFFER_LEN 2048
 
 
 void extract_buildings(vector<string>&buildings, string path){
@@ -53,15 +60,69 @@ int main(int argc, char* argv[]){
 
     cout << BOLD_YELLOW << "Amount of Buildings Found : " << EFFECT_END << BOLD_GREEN << Buildings.size() << EFFECT_END << endl << endl;
     cout << BOLD_RED << "Buildings' Names are as follow : " << EFFECT_END << endl;
-    for(int i = 0; i < Buildings.size(); i++){
+    for(int i = 0; i < Buildings.size(); i++)
         cout << UNDERLINED_WHITE << Buildings[i] << EFFECT_END << ", ";
-    }
     cout << endl << endl << BOLD_GREEN << "Enter the resources and buildings you are looking for : " << EFFECT_END << endl;
+    
     string input_line;
     getline(cin, input_line); //Gets space-seperated input from user
     vector<string> resource_requests = get_requests(input_line);
     getline(cin, input_line);
     vector<string> building_requests = get_requests(input_line); //Assumption -> resource and building requests are mapped 1 by 1
+
+    //Creating child_processe and pipe for loading resource prices
+    int bills_pipe[2];
+    if(pipe(bills_pipe) == -1){
+        exit(EXIT_FAILURE);
+    }
+    pid_t pid = fork();
+    if(pid == 0){
+        close(bills_pipe[READ_PIPE]);
+        dup2(bills_pipe[WRITE_PIPE], STDOUT_FILENO);
+        char* exec_path = const_cast<char*>("./bills.o");
+        char* exec_arg[] = {NULL};
+        execvp(exec_path, exec_arg);
+    }
+    else{
+        close(bills_pipe[WRITE_PIPE]);
+    }
+
+    //Creating child processes and their pipes for each requested building
+    int main_pipes[building_requests.size()][2];
+    for(int i = 0; i < building_requests.size(); i++){
+        if(pipe(main_pipes[i]) == -1){
+            exit(EXIT_FAILURE);
+        }
+        pid = fork();
+        if(pid == 0){
+            close(main_pipes[i][READ_PIPE]);
+            dup2(main_pipes[i][WRITE_PIPE], STDOUT_FILENO);
+            if(resource_requests.size() == 1){
+                char* exec_path = const_cast<char*>("./building.o");
+                char* exec_arg[] = {const_cast<char*>(building_requests[i].c_str()), const_cast<char*>(resource_requests[0].c_str()), NULL};
+                execvp(exec_path, exec_arg);
+            }
+            else if(resource_requests.size() == 2){
+                char* exec_path = const_cast<char*>("./building.o");
+                char* exec_arg[] = {const_cast<char*>(building_requests[i].c_str()), const_cast<char*>(resource_requests[0].c_str()), const_cast<char*>(resource_requests[1].c_str()), NULL};
+                execvp(exec_path, exec_arg);
+            }
+            else {
+                char* exec_path = const_cast<char*>("./building.o");
+                char* exec_arg[] = {const_cast<char*>(building_requests[i].c_str()), const_cast<char*>(resource_requests[0].c_str()), const_cast<char*>(resource_requests[1].c_str()),  const_cast<char*>(resource_requests[2].c_str()), NULL};
+                execvp(exec_path, exec_arg);
+            }
+        }
+        else {
+            close(main_pipes[i][WRITE_PIPE]);
+        }
+    }
+
+    char buffer[BUFFER_LEN];
+
+    for(int i = 0; i < building_requests.size(); i++){
+        //TO DO
+    }
 
 
     return 0;
